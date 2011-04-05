36bf113873ea4a9620ea7b976d8bcc7a9564105d
Carl Zulauf <czulauf@lyconic.com>
2011-04-05 17:24:47 -0500

    More search improvements, plus improved error handling in a couple scenarios.

--- songbus.rb
+++ songbus.rb
@@ -18,15 +18,24 @@ artist = song['artist']; title = song['title']
 
 puts "\n#{artist} - #{title}:\n\n"
 
-query = "\"#{artist}\" \"#{title}\" lyrics site:songmeanings.net"
+query = "#{artist} \"#{title}\" lyrics site:songmeanings.net"
 
-search = RubyWebSearch::Google.search(:query => query, :size => 1)
+search = RubyWebSearch::Google.search(:query => query, :size => 10)
 
-if search.results.first.nil?
+lyrics_url = nil
+search.results.each do |result|
+  if result[:url] =~ /songmeanings\.net\/songs\/view\//
+    lyrics_url = result[:url]
+    break
+  end
+end
+
+if lyrics_url.nil?
   puts "\n-- NO LYRICS FOUND --\n"
+  puts "You might find some at this URL: #{search.results.first[:url]}\n" unless search.results.first.nil?
 else
-  puts "#{search.results.first[:url]}\n\n"
-  raw_html = Net::HTTP.get(URI(search.results.first[:url]))
+  puts "#{lyrics_url}\n\n"
+  raw_html = Net::HTTP.get(URI(lyrics_url))
   if raw_html.nil?
     puts "\n-- UNABLE TO FETCH LYRICS FOR THIS SONG --\n"
   else
